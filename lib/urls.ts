fde62cc98aca9ba8ca1a45de9fa63cbcf49c8ea2
Tal Glanzman <talglanzman@gmail.com>
2023-02-15 20:50:40 +0200

    fix attempt

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,5 +1,9 @@
-export const home = () => '';
-export const taxonomyArticles = (taxonomy: string, value: string) => `_${taxonomy.toLowerCase()}/${value}`;
+import config from '../config.json';
+
+const url = (...values: string[]) => values.join("/");
+
+export const home = () => config.site.homePath;
+export const taxonomyArticles = (taxonomy: string, value: string) => url(home(), `_${taxonomy.toLowerCase()}`, value);
 export const tagArticles = (tag: string) => taxonomyArticles('tags', tag);
 export const categroyArticles = (category: string) => taxonomyArticles('categories', category);
-export const article = (articleId: string) => `_articles/${articleId}`;
+export const article = (articleId: string) => url(home(), '_articles', articleId);
